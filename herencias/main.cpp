4b45c56ae8c34ac9dbdea61cccb3c73232d1ca97
Andres D Osorio L <38448621+andavol96@users.noreply.github.com>
2018-05-07 15:19:41 -0400

    add herencia

---
+++ herencias/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Cuerpo.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	Cuerpo* cuerpo = new Cuerpo();
+	cuerpo->saludar();
+	return 0;
+}
